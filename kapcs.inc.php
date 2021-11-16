de92089a1cde20747b0463ccaf72b2bc7e128404
Aronvecs <aronvecsernyes@gmail.com>
2021-11-16 10:20:22 +0100

    kapcsolat

---
+++ kapcs.inc.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("localhost","root","","tanulok");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  print( "Adatbázis kapcsolódási hiba!  " . mysqli_connect_error());
+  }
+
+
+
+ 
+ 
+ 
+ 
+  
+?>
